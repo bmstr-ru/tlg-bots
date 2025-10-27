340dba0b035a5a91e7960a190750003333b285eb
Dmitry Ermashev <dima@bmstr.ru>
2025-10-28 00:31:49 +0300

    Post torgi to specific topic

--- torgi-bot/telegram.go
+++ torgi-bot/telegram.go
@@ -3,24 +3,27 @@ package main
 import (
 	"bytes"
 	"context"
-	"github.com/go-telegram/bot"
-	"github.com/go-telegram/bot/models"
 	"os"
 	"strconv"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
 )
 
 func NotifyTelegram(lot *Lot) error {
-	b, err := bot.New(os.Getenv("TELEGRAM_TOKEN"))
+	b, err := bot.New(os.Getenv("TG_BOT_TOKEN"))
 	if err != nil {
 		return err
 	}
 
-	chatId, err := strconv.ParseInt(os.Getenv("TELEGRAM_CHAT_ID"), 10, 64)
+	chatId, _ := strconv.ParseInt(os.Getenv("TG_CHANNEL_ID"), 10, 64)
+	threadId, _ := strconv.ParseInt(os.Getenv("TG_THREAD_ID"), 10, 64)
 	if lot.Image != nil {
 		_, err = b.SendPhoto(context.TODO(), &bot.SendPhotoParams{
-			ChatID:  chatId,
-			Photo:   &models.InputFileUpload{Data: bytes.NewReader(lot.Image)},
-			Caption: "https://torgi.gov.ru/new/public/lots/lot/" + lot.Id,
+			ChatID:          chatId,
+			MessageThreadID: int(threadId),
+			Photo:           &models.InputFileUpload{Data: bytes.NewReader(lot.Image)},
+			Caption:         "https://torgi.gov.ru/new/public/lots/lot/" + lot.Id,
 		})
 	}
 
